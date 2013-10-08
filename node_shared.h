2694eafea04ac5b7d585ff93d2a1dc1f4dbf8088
Sam Mikes <smikes@cubane.com>
2013-10-08 15:56:33 -0600

    ensure NON-use of dlmalloc

--- node_shared.h
+++ node_shared.h
@@ -2,58 +2,10 @@
 #ifndef NODE_SHARED_H
 #define NODE_SHARED_H
 
-/******************************
- Which Allocator Should We Use?
- ******************************/
-
-#if defined(_DEBUG)
-/* In debug mode, we want the MSFT allocator for its leak-checking code */
-//#define USE_DL_MALLOC 0
-#else
-/* In release mode, we want the DL allocator for its superior efficiency */
-//#define USE_DL_MALLOC 0
-#endif
-
-//uncomment below to debug the dlmalloc-using code
-//#define DEBUG_DL_MALLOC 1
-
-#ifdef DEBUG_DL_MALLOC
-#define USE_DL_MALLOC 1
-#endif
-
-/* if we have the DL allocator */
-#if defined(USE_DL_MALLOC)
-
-/* undefine the basic memory function macros (if any) */
-#ifdef malloc
-#undef malloc
-#endif
-#ifdef free
-#undef free
-#endif
-#ifdef calloc
-#undef calloc
-#endif
-#ifdef realloc
-#undef realloc
-#endif
-
-/* define the basic memory function macros to refer to dlmalloc functions */
-#define malloc(n)			dlmalloc((n))
-#define free(p)				dlfree((p))
-#define calloc(n,s)			dlcalloc((n),(s))
-#define realloc(p,n)		dlrealloc((p),(n))
-
-/* include the dlmalloc library header */
-#include "node_dlmalloc.h"
-
-#else
-
 /* using the MSFT allocator */
 #define _CRTDBG_MAP_ALLOC 1
 #include <crtdbg.h>
 
-#endif
 
 #ifdef __cplusplus
 extern "C" {
