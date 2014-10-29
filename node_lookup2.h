175f7be41aeea20b7f8312c1bc29ad6836bbc5f9
smikes <smikes@cubane.com>
2014-10-29 10:26:05 -0600

    use uint32_t names for unsigned byte/word

--- node_lookup2.h
+++ node_lookup2.h
@@ -1,8 +1,8 @@
 #ifndef LOOKUP2_H
 #define LOOKUP2_H
 
-typedef  unsigned __int32 ub4;   /* unsigned 4-byte quantities */
-typedef  unsigned __int8  ub1;
+typedef uint32_t ub4;   /* unsigned 4-byte quantities */
+typedef uint8_t  ub1;
 
 #define hashsize(n) ((ub4)1<<(n))
 #define hashmask(n) (hashsize(n)-1)
